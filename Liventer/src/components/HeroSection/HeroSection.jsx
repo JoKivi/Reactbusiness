9b572713a7ae3bbad85ebfc8b3da9f7f781e6bea
Mixu <saviojamicaela@gmail.com>
2024-01-20 20:06:11 +0200

    FadeEffect siirretty omaksi komponentiksi

--- Liventer/src/components/HeroSection/HeroSection.jsx
+++ Liventer/src/components/HeroSection/HeroSection.jsx
@@ -1,32 +1,10 @@
-import React, { useEffect, useRef } from 'react';
+import { useRef } from 'react';
+import FadeEffect from '../../Styles/FadeEffect';
 import './HeroSection.css';
 
 function HeroSection() {
     const heroTextRef = useRef(null);
-
-    useEffect(() => {
-        const checkIfInView = () => {
-            const element = heroTextRef.current;
-            if (!element) return;
-
-            const elementTop = element.getBoundingClientRect().top;
-
-            // Tarkista onko sivua vieritetty vähintään 20px
-            if (window.scrollY > 0) {
-                if (elementTop < window.innerHeight && elementTop >= -50) {
-                    element.classList.add('animate');
-                } else {
-                    element.classList.remove('animate');
-                }
-            }
-        };
-
-        window.addEventListener('scroll', checkIfInView);
-
-        checkIfInView();
-
-        return () => window.removeEventListener('scroll', checkIfInView);
-    }, []);
+    FadeEffect(heroTextRef);
 
     return (
         <section className="hero" id="hero">
