92a32543b2e9ddecece7d5b56713ef6a77270309
JoKivi <joni.kivimaki@elisanet.fi>
2024-01-25 20:11:03 +0200

    Regon luotu

---
+++ Liventer/src/pages/Regon.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Pages.css';
+
+function Regon() {
+    return (
+        <div className="ohjelma-container">
+            <div className="ohjelma-image">
+                <img src="/img/regon.webp" alt="dj Regon" />
+            </div>
+            <div className="ohjelma-content">
+                <h2>DJ REGON</h2>
+                <br />
+                <p>Dj Regon on hyperenerginen esiintyjä niin klubeille, kuin festarilavoille. 
+                Regon on monipuolinen musiikillisesti ja yhdistelee taitavasti eri musiikkityylejä. 
+                Keikoilla ei koskaan voi tietää, mitä tapahtuu seuraavaksi ja ilta tuo tullessaan aina 
+                jotain uutta. Mukaansa tempaava meininki saa sinut nostamaan bilevaihteen päälle ja 
+                juhlat tulevat olemaan ikimuistoiset.
+                Regon on pitkänlinjan dj kahden vuosikymmenen kokemuksella ja esiintynyt paljon 
+                erilaisissa tapahtumissa ja juhlissa niin maalla, kuin merellä.
+                </p>
+                <br /><br />
+                    <p><strong>Kun haluat joraa kunnolla, tilaa Dj Regon ! !</strong></p>
+            </div>
+        </div>
+    );
+}
+
+export default Regon;
