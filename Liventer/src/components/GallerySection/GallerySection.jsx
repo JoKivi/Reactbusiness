ef1e9c0e9971b8441ea8a8ae547d68cfa58abb76
Mixu <saviojamicaela@gmail.com>
2024-01-31 17:08:40 +0200

    gallerySection hienosäätöä

--- Liventer/src/components/GallerySection/GallerySection.jsx
+++ Liventer/src/components/GallerySection/GallerySection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as LinkRouter } from 'react-router-dom'
-import './gallery.css'
+import './gallerySection.css'
 
 import img0 from '../../images/img0.webp'
 import img1 from '../../images/img1.webp'
@@ -14,8 +14,14 @@ import img8 from '../../images/img2.webp'
 import img9 from '../../images/img3.webp'
 import img10 from '../../images/img4.webp'
 import img11 from '../../images/img5.webp'
+import img12 from '../../images/img0.webp'
+import img13 from '../../images/img1.webp'
+import img14 from '../../images/img2.webp'
+import img15 from '../../images/img3.webp'
+import img16 from '../../images/img4.webp'
+import img17 from '../../images/img5.webp'
 
-const gallerySectionImages = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11]
+const gallerySectionImages = [img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17]
 
 const Slider = () => {
     return (
@@ -31,9 +37,12 @@ const GallerySection = () => {
     return (
         <div name='gallerySection' className='gallerySection'>
                 <Slider/>
-                <LinkRouter to='/gallery' className='gallerySectionBtn'>
-                    <button>Avaa Galleria</button>
-                </LinkRouter>
+                <div className="gallerySectionBtn">
+                    <LinkRouter to='/gallery'>
+                        <button>Avaa Galleria</button>
+                    </LinkRouter>
+                </div>
+
         </div>
     );
 }
