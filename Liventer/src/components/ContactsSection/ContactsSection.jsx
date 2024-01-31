722813f1c13bb0c85b88cbb14e81158ef2b76049
JoKivi <joni.kivimaki@elisanet.fi>
2024-01-31 14:09:25 +0200

    Contacts update

--- Liventer/src/components/ContactsSection/ContactsSection.jsx
+++ Liventer/src/components/ContactsSection/ContactsSection.jsx
@@ -7,19 +7,28 @@ const ContactsSection = () => {
             <div className="contact">
                 <img src="/img/Djsmiley.webp" alt="Contact 1" className="contact-image" />
                 <div className="contact-info">
-                    <h2>Contact 1</h2>
-                    <p><strong>Title 1</strong></p>
-                    <p>contact1@example.com</p>
-                    <p>+1 123-456-7890</p>
+                    <h2>OKKO IKONEN</h2>
+                    <p><strong>Toimitusjohtaja</strong></p>
+                    <p>okko.ikonen@liventer.fi</p>
+                    <p>+358 50 3566 924</p>
+                </div>
+            </div>
+            <div className="contact">
+                <img src="/img/Samppa.webp" alt="Contact 2" className="contact-image" />
+                <div className="contact-info">
+                    <h2>SAMPPA SILVAANI</h2>
+                    <p><strong>Tuotantopäällikkö</strong></p>
+                    <p>samppa@liventer.fi</p>
+                    <p>+358 40 5455 417</p>
                 </div>
             </div>
             <div className="contact">
                 <img src="/img/Djsmiley.webp" alt="Contact 2" className="contact-image" />
                 <div className="contact-info">
-                    <h2>Contact 2</h2>
-                    <p><strong>Title 2</strong></p>
-                    <p>contact2@example.com</p>
-                    <p>+1 987-654-3210</p>
+                    <h2>KIMMO LUOKKANEN</h2>
+                    <p><strong>Aluevastaava pohjoinen</strong></p>
+                    <p>kimmo.luokkanen@liventer.fi</p>
+                    <p>+358050 3823 211</p>
                 </div>
             </div>
         </div>
