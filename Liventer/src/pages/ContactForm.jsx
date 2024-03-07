02f397fb240738b585bb6413badbea1b8cbb7af2
JoKivi <joni.kivimaki@elisanet.fi>
2024-03-07 10:51:49 +0200

    Sähköpostin rakennusta

--- Liventer/src/pages/ContactForm.jsx
+++ Liventer/src/pages/ContactForm.jsx
@@ -18,9 +18,38 @@ function ContactForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData); //Konsolissa näkyy toimivan. Eri asia sitten sen backendin kanssa
-        setFormData({ name: '', email: '', message: '' });
+    
+        const url = 'http://liventer.fi/home/gvyzlrgc/public_html/check_contact.php';
+    
+        const formDataToSend = new FormData();
+        formDataToSend.append('user_name', formData.name);
+        formDataToSend.append('email', formData.email);
+        formDataToSend.append('subject', formData.subject);
+        formDataToSend.append('message', formData.message);
+    
+        fetch(url, {
+            method: 'POST',
+            body: formDataToSend,
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text(); 
+        })
+        .then(data => {
+            console.log('Success:', data);
+            // Käsittele onnistunut palvelimen vastaus täällä
+            // Esimerkiksi näytä onnistumisviesti käyttäjälle tai ohjaa toiselle sivulle
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            // Käsittele virhetilanteita täällä
+        });
+    
+        setFormData({ name: '', email: '', subject: '', message: '' });
     };
+    
 
     return (
         <div className="contact-form-container">
